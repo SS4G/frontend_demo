64865ad9b7814cf88d8f9974607c0f9f5b2c399a
songzihengs.666 <songziheng.666@bytedance.com>
2023-08-06 17:00:26 +0800

    [feat] element_ui 基础布局组件

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+//import HelloWorld from './components/HelloWorld.vue'
+//import ElementButton from './components/ElementButton.vue'
+import ElementContainer from './components/ElementContainer.vue'
+//import ElementLayout from './components/ElementLayout.vue'
+
+</script>
+
+<template>
+  <div>
+    <ElementContainer/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ElementContainer.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="common-layout">
+      <el-container >
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-container>
+            <el-header>Header</el-header>
+            <el-main>Main</el-main>
+            <el-footer>Footer</el-footer>
+          </el-container>
+        </el-container>
+      </el-container>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped> 
+.el-container {
+    height: 100vh;
+    width: 100vw;
+}
+.el-header {
+    background-color: aqua;
+    height: 20%;
+}
+
+.el-main {
+    background-color: rgb(25, 0, 255);
+    height: 70%;
+}
+
+.el-footer {
+    background-color: rgb(0, 255, 60);
+    height: 10%;
+}
+
+.el-aside {
+    background-color: rgb(255, 0, 144);
+    width: 20%;
+}
+
+</style>
